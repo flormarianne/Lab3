8c55bbdc920b59fb449fee6bb412b5d800bfc213
flormarianne <2015081647@ust-ics.mygbiz.com>
2018-09-22 17:46:25 +0800

    Initial commit

---
+++ app/src/main/java/flor/pacatang/com/lab3/PacatangFlorLab3.java
@@ -0,0 +1,13 @@
+package flor.pacatang.com.lab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PacatangFlorLab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pacatang_flor_lab3);
+    }
+}
